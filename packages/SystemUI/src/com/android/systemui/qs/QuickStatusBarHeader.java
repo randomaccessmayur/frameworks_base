affede466d3db5320fd94d1d97e3109f1039311f
SagarMakhar <sagarmakhar@gmail.com>
2021-10-27 08:45:10 +0000

    SystemUI: Fix paddings
    - Set margin for lockscreen multi user avatar
    - Set marging for qs no_carrier_text
    - Don't set cutout paddings for qs clock
    
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -502,7 +502,7 @@ public class QuickStatusBarHeader extends FrameLayout
         boolean hasCornerCutout = mInsetsProvider.currentRotationHasCornerCutout();
 
         mDatePrivacyView.setPadding(sbInsets.first, 0, sbInsets.second, 0);
-        mStatusIconsView.setPadding(sbInsets.first, 0, sbInsets.second, 0);
+        //mStatusIconsView.setPadding(sbInsets.first, 0, sbInsets.second, 0);
         LinearLayout.LayoutParams datePrivacySeparatorLayoutParams =
                 (LinearLayout.LayoutParams) mDatePrivacySeparator.getLayoutParams();
         LinearLayout.LayoutParams mClockIconsSeparatorLayoutParams =
@@ -591,10 +591,12 @@ public class QuickStatusBarHeader extends FrameLayout
                 mWaterfallTopInset,
                 paddingRight,
                 0);
+/*
         mStatusIconsView.setPadding(paddingLeft,
                 mWaterfallTopInset,
                 paddingRight,
                 0);
+*/
     }
 
     public void updateEverything() {
