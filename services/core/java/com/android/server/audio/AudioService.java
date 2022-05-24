fc693637c0011776e45c5c7e62357ac27fae9b3d
ProchyGaming <dominikprochazka100@gmail.com>
2022-05-24 21:02:08 +0200

    base: Mute media only on silent [1/2]
    This is how its done on OOS and I think its better to apply it only on silent as well
    
    f
    
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -4825,6 +4825,7 @@ public class AudioService extends IAudioService.Stub
         final int ringerMode = mRingerMode; // Read ringer mode as reading primitives is atomic
         final boolean ringerModeMute = ringerMode == AudioManager.RINGER_MODE_VIBRATE
                 || ringerMode == AudioManager.RINGER_MODE_SILENT;
+        final boolean ringerModeSilent = ringerMode == AudioManager.RINGER_MODE_SILENT;
         final boolean shouldRingSco = ringerMode == AudioManager.RINGER_MODE_VIBRATE
                 && mDeviceBroker.isBluetoothScoActive();
         // Ask audio policy engine to force use Bluetooth SCO channel if needed
@@ -4874,7 +4875,7 @@ public class AudioService extends IAudioService.Stub
             }
         }
         if (mRingerMuteSpeakerMedia) {
-            if (ringerModeMute) {
+            if (ringerModeSilent) {
                 // Set volume to 0 instead of muting because we won't want to
                 // affect other devices under same type
                 setStreamVolumeInt(AudioSystem.STREAM_MUSIC, 0,
@@ -4911,7 +4912,8 @@ public class AudioService extends IAudioService.Stub
             change = mRingerMode != ringerMode;
             // Save current media volume if previous ringer mode is normal
             if (mRingerMuteSpeakerMedia && change
-                    && mRingerMode == AudioManager.RINGER_MODE_NORMAL) {
+                    && (mRingerMode == AudioManager.RINGER_MODE_NORMAL ||
+                        mRingerMode == AudioManager.RINGER_MODE_VIBRATE)) {
                 mSavedSpeakerMediaIndex =
                     mStreamStates[AudioSystem.STREAM_MUSIC]
                         .getIndex(AudioSystem.DEVICE_OUT_SPEAKER);
