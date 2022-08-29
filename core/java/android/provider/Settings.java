b53cc41c74cc79a04aa468ed9664b7c533348b4a
StarkDroid <gamerprince.exp@gmail.com>
2022-08-29 20:49:52 +0200

    [1/2] Statusbar Clock background chip
    Inspired by looking at few magisk/substratum module :P
    @ProchyGaming Settings observer imported from
    5ce1d7cc7e261576083043ed039211fc6647d626
    
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6030,6 +6030,14 @@ public final class Settings {
         public static final String ALERT_SLIDER_PULSE = "alert_slider_pulse";
 
         /**
+         * Statusbar clock background
+         * 0 - hide accented chip 
+         * 1 - show accented chip (default)
+         * @hide
+         */
+        public static final String STATUSBAR_CLOCK_CHIP = "statusbar_clock_chip";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
