7cc64c6eaa506c00806b02d7057a6fa8f7898cbf
minaripenguin <minaripenguin@users.noreply.github.com>
2023-11-08 14:27:01 +0800

    VolumeDialog: Ensure proper resource release
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -544,6 +544,13 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
 
     private void initDialog(int lockTaskModeState) {
         Log.d(TAG, "initDialog: called!");
+        if (mDialog != null) {
+            mDialog.dismiss();
+            mDialog = null;
+        }
+        if (mConfigurableTexts != null) {
+            mConfigurableTexts = null;
+        }
         mDialog = new CustomDialog(mContext);
         initDimens();
 
@@ -1647,7 +1654,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
         mHandler.removeMessages(H.DISMISS);
         rescheduleTimeoutH();
 
-        if (mConfigChanged) {
+        if (mDialog == null) {
             initDialog(lockTaskModeState); // resets mShowing to false
             mConfigurableTexts.update();
             mConfigChanged = false;
@@ -1762,6 +1769,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
                     if (mDialog != null) {
                         mDialog.dismiss();
                     }
+                    mDialog = null;
                     tryToRemoveCaptionsTooltip();
                     mDefaultRow = null;
                     mIsAnimatingDismiss = false;
